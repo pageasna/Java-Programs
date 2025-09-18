ae642d9dddd814991cdbb749550ff214a8c818df
ASNA MIRZA <asnamirza2020@gmail.com>
2025-09-18 19:08:02 +0530

    Add files via upload

---
+++ Java-Programs/WEEK 3/Week3/src/main/java/com/asnadocs/week3/AverageOf3Numbers.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.asnadocs.week3;
+
+/**
+ *
+ * @author ASUS
+ */
+public class AverageOf3Numbers {
+    public static void main(String[] args) {
+        float num1=10;
+        float num2=20;
+        float num3=30;
+        float average= (num1 + num2 + num3) / 3;
+        System.out.println("Average of 3 numbers:" + average);
+    }
+    
+}
---
+++ Java-Programs/WEEK 3/Week3/src/main/java/com/asnadocs/week3/Swap2Numbers.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.asnadocs.week3;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Swap2Numbers {
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        System.out.println("Before swapping:");
+        System.out.println("a=" +a+ ",b=" +b);
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After Swapping:");
+        System.out.println("a=" +a+ ",b=" +b);
+    }
+    
+}
